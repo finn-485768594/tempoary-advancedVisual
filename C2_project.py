ae17cbb6125cd84ecd81c9f7bedcd85cbddfb64d
Finn pc <finnmcgeehin@gmail.com>
2025-11-20 20:24:30 +0000

    added octaves

--- C2_project.py
+++ C2_project.py
@@ -23,21 +23,41 @@ print(f"test images array has {len(testImagesarray)} images of size {testImagesa
 
 #now the data is loaded, build gaussian pyramids for the icons
 class libarygaussianPyramid:
-    def __init__(self, image, levels):
+    def __init__(self, image, levels=5,octaves=3):
         self.levels = levels
-        self.pyramid = [image] #as i prefer appending my pyramid is going to be upside down if it gets printed but it works the exact same
+        self.octaves = octaves
+        self.pyramid = [[image]] #as i prefer appending my pyramid is going to be upside down if it gets printed but it works the exact same
         self.build_pyramid()
 
     def build_pyramid(self):
+        image=self.pyramid[0][0]
+        this_level = [image]
+        for j in range(1, (self.octaves+1)): 
+            #print(j)
+            this_level.append(cv2.GaussianBlur(image, (5, 5), sigmaX=j*1.5, sigmaY=j*1.5))
+        print(f"Level 0 has {len(this_level)} images of size {this_level[0].shape}")
+        self.pyramid = []  # Reset pyramid to empty list
+        self.pyramid.append(this_level)
+        print(f"Pyramid now has {len(self.pyramid)} levels")
+        
         for i in range(1, self.levels):
-            image = cv2.pyrDown(self.pyramid[i - 1])
+            image = cv2.pyrDown(self.pyramid[i - 1][0])
             #note to self: cv2.pyrDown 
             #Applies a 5×5 Gaussian blur to the image
             #Downsamples it by a factor of 2 in width and height
-            self.pyramid.append(image)
+            this_level = [image]
+            #print("test level")
+            for j in range(1, (self.octaves+1)): 
+                #print(j)
+                this_level.append(cv2.GaussianBlur(image, (5, 5), sigmaX=j*1.5, sigmaY=j*1.5))
+            print(f"Level {i} has {len(this_level)} images of size {this_level[0].shape}")
+            self.pyramid.append(this_level)
+            print(f"Pyramid now has {len(self.pyramid)} levels")
+        
+
     
     def redefine_pyramid(self, image):
-        self.pyramid = [image]
+        self.pyramid = [[image]]
         self.build_pyramid()
 
     def get_level(self, level):
@@ -48,11 +68,18 @@ class libarygaussianPyramid:
         
     def get_pyramid(self):
         return self.pyramid
-        
+
+    '''
     def show_pyramid_test(self):
+        print(len(self.pyramid))
+        print(len(self.pyramid[0]))
         #this function was made using github copilot. make sure to make a different version before submission as this doesnt have the functinoality i want but is used for testing
-        
-        rgba_images = [cv2.cvtColor(img, cv2.COLOR_BGRA2RGBA) for img in self.pyramid]
+        rgba_images=[]
+        for i in range(len(self.pyramid)):
+            #rgba_images.append(cv2.cvtColor(self.pyramid[i][0], cv2.COLOR_BGRA2RGBA))
+            for j in range(len(self.pyramid[i])):
+                rgba_images.append(cv2.cvtColor(self.pyramid[i][j], cv2.COLOR_BGRA2RGBA))
+        #rgba_images = [cv2.cvtColor(img, cv2.COLOR_BGRA2RGBA) for img in self.pyramid]
 
         cols = len(rgba_images)
         plt.figure(figsize=(3 * cols, 4))
@@ -78,7 +105,42 @@ class libarygaussianPyramid:
             ax.axis("off")
 
         #print("test live #c")
-       
+    '''
+
+    def show_pyramid_test(self):
+        print("Levels:", len(self.pyramid))
+        print("Images per level:", [len(row) for row in self.pyramid])
+
+        rows = len(self.pyramid)
+        cols = max(len(row) for row in self.pyramid)
+
+        plt.figure(figsize=(4 * cols, 4 * rows))
+
+        for i in range(rows):            # pyramid level
+            for j in range(len(self.pyramid[i])):   # images inside level
+
+                img = self.pyramid[i][j]
+                img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGBA)
+
+                # Background color depends only on the pyramid level
+                bg_color = np.array([0, 0, (i * (255 / rows))], dtype=np.uint8)
+
+                h, w = img.shape[:2]
+                background = np.ones((h, w, 3), dtype=np.float32) * bg_color
+
+                # Foreground RGB and alpha
+                fg = img[..., :3].astype(np.float32)
+                alpha = img[..., 3:4].astype(np.float32) / 255.0
+
+                # Composite foreground over background
+                comp = fg * alpha + background * (1 - alpha)
+
+                ax = plt.subplot(rows, cols, i * cols + j + 1)
+                ax.imshow(comp.astype(np.uint8))
+                ax.set_title(f"Level {i}, Img {j}")
+                ax.axis("off")
+
+     
 
     
 
